b932009b1afc7c942d33fa22872c902f47087fdd
Supriyo Barua Shaown <Supriyo@ZeroTwo-2.local>
2024-12-14 12:25:48 +0600

    deleted some useless files

--- Funtions/basic.cpp
+++ Funtions/basic.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void func(){
-    cout <<"Ami tumi ar amra cholo jai kothao";
+void func()
+{
+    cout << "Ami tumi ar amra cholo jai kothao";
 }
-int main(){
+int main()
+{
     func();
-
 }
