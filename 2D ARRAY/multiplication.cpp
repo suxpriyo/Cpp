dee80ff68c120db4b7e1f7dfc99d5812b29cc143
suxpriyo <supriyobaruab@gmail.com>
2024-12-14 09:19:56 +0600

    added the modified file

--- 2D ARRAY/multiplication.cpp
+++ 2D ARRAY/multiplication.cpp
@@ -16,17 +16,14 @@ int main()
     {
         for (int j = 0; j < 2; j++)
         {
-            sum = sum + (a[i][j] * b[j][i]);
+            c[i][j] = (a[i][j] * b[j][i]); // can use "+" for addition and "-" for subtraction and "/" for division
         }
-        cout << sum << endl;
-        // c[i][j] = sum;
-        sum = 0;
     }
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            // cout << c[i][j] << "\t";
+            cout << c[i][j] << "\t";
         }
         cout << "\n";
     }
